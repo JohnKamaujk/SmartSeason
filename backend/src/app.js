e8bdb42bc4d7b102513eb2835ba8aad8e881aaa5
JohnKamaujk <johnjkkamau@gmail.com>
2026-04-23 11:00:16 +0300

    fix: Cors fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,11 @@ const userRoutes = require("./routes/user.routes");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  }),
+);
 app.use(express.json());
 
 app.use("/auth", authRoutes);
